e77552443fb298ea41b82fbf1035e0cf43d574c9
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-22 15:52:32 -0500

    Use diferent routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import {Navbar} from '../components/ui/Navbar'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
 } from "react-router-dom";
 import { LoginScreen } from '../components/login/LoginScreen';
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
-                <Navbar />
+                {/* <Navbar /> */}
 
                 <Switch>
                     <Route exact path="/login" component={LoginScreen}/>
-                    <Route exact path="/" component={ MarvelScreen }/>
+                    <Route path="/" component={ DashboardRoutes }/>
                 </Switch>
             </div>
         </Router>
