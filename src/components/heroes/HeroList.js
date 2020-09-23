741a43f47daa36e7dbeb6a0049da0ff83dc5a638
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-22 20:34:09 -0500

    HeroScreen made with specific id, and use of useMemo for optimizations

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import {getHeroesByPublisher} from '../../selectors/getHeroesByPublisher'
 import { HeroCard } from './HeroCard'
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher(publisher)
+    // const heroes = getHeroesByPublisher(publisher)
+    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
+
     return (
         <ul className="card-columns">
             {
