22713a93b737624b43eae0d5a55fb8911ea561a9
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-25 19:44:40 -0500

    Docs folder added for a demo

---
+++ docs/precache-manifest.5140d01ba1332cbd0990d6e3042aea5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5f91d812b7a1f3d6e6a3dc7404bdb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3919aebab2660d1df4ad",
+    "url": "/static/js/2.34bbaa86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.34bbaa86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50c4240ad9edbe7cebd",
+    "url": "/static/js/main.c4aa5f1a.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
